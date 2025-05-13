a5e4f0704ae7bbc660a0ff0a3cb0026f07273917
Naurah Mufidah Santoso <2400018111@webmail.uad.ac.id>
2025-05-13 13:00:52 +0700

    JavaScript
    Source Code JS

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const name = "Saya";
+    console.log("Selamat datang di website " + name + "!");
+    alert("Hallo Selamat Datang di Profil Saya!");
+  });
